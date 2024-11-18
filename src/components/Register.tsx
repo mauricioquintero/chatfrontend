22b356c2cb0dd7be0924a7fd5131750e74cb3d40
mauricio <mau.quinterog@gmail.com>
2024-11-18 11:58:26 -0600

    Updated visuals to register component

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Register: React.FC = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleRegister = async () => {
         try {
@@ -15,31 +17,51 @@ const Register: React.FC = () => {
         }
     };
 
+    const handleLoginRedirect = () => {
+        navigate('/login');
+    }
+
     return (
-        <div className="max-w-md mx-auto p-4">
-            <h2 className="text-2xl mb-4">Register</h2>
-            {message && <p>{message}</p>}
-            <input
-                className="border p-2 w-full mb-2"
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-            />
-            <input
-                className="border p-2 w-full mb-2"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-            />
-            <button
-                className="bg-blue-500 text-white p-2 w-full"
-                onClick={handleRegister}
-            >
-                Register
-            </button>
+        <div className="flex items-center justify-center min-h-screen bg-draculabackground px-4">
+            <div className="bg-draculacurrentline shadow-md rounded-lg p-8 w-full max-w-md sm:w-96">
+                <h2 className="text-xl font-semibold mb-4 text-draculaforeground text-center sm:text-left">Register</h2>
+                {message && <p>{message}</p>}
+                <label className="block text-draculaforeground text-sm sm:text-base">Username</label>
+                <input
+                    className="bg-draculaforeground border border-gray-300 rounded-md p-2 w-full mb-4"
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+
+                <label className="block text-draculaforeground text-sm sm:text-base">Password</label>
+                <input
+                    className="bg-draculaforeground border border-gray-300 rounded-md p-2 w-full mb-4"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button
+                    className="hover:opacity-90 bg-draculagreen text-white p-2 w-full rounded-md font-semibold sm:text-base text-sm"
+                    onClick={handleRegister}
+                >
+                    Register
+                </button>
+
+                <div className="text-center mt-4">
+                    <button
+                        onClick={handleLoginRedirect}
+                        className="hover:opacity-90 text-draculaforeground underline text-sm sm:text-base"
+                    >
+                        Already registered? Sign in here
+                    </button>
+                </div>
+            </div>
         </div>
+
+
     );
 };
 
